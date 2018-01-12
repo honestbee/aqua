9fb7f405a4f19677990fc90111cf59ae8c5dea3d
Stewart Wu <wys1203@gmail.com>
2018-01-12 18:16:36 +0800

    integrate pprof (#1)

--- rest_server.go
+++ rest_server.go
@@ -3,6 +3,7 @@ package aqua
 import (
 	"fmt"
 	"net/http"
+	"net/http/pprof"
 	"reflect"
 	"strings"
 	"time"
@@ -37,13 +38,29 @@ type RestServer struct {
 	auth   Authorizer
 }
 
+func AttachProfiler(router *mux.Router) {
+	router.HandleFunc("/debug/pprof/", pprof.Index)
+	router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	router.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	router.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+
+	// Manually add support for paths linked to by index page at /debug/pprof/
+	router.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+	router.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+	router.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
+	router.Handle("/debug/pprof/block", pprof.Handler("block"))
+}
+
 func NewRestServer() RestServer {
+	pprofRouter := mux.NewRouter()
+	AttachProfiler(pprofRouter)
+
 	r := RestServer{
 		Fixture: defaults,
 		Server:  http.Server{},
 		Port:    defaultPort,
 		svcs:    make([]interface{}, 0),
-		mux:     mux.NewRouter(),
+		mux:     pprofRouter,
 		apis:    make(map[string]endPoint),
 		mods:    make(map[string]func(http.Handler) http.Handler),
 		stores:  make(map[string]cache.Cacher),
